861e0ee29821f064c8109a1d3986cb5072c89a7d
duplt <phanlamthanhdu@gmail.com>
2025-12-01 13:59:14 +0700

    Add files

--- tasks/s3_upload.py
+++ tasks/s3_upload.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import os
 
 def upload_file_to_s3(data: dict, bucket_name: str, file_name: str) -> bool:
     """
@@ -15,8 +16,12 @@ def upload_file_to_s3(data: dict, bucket_name: str, file_name: str) -> bool:
     """
 
     # Create an S3 client
-    s3_client = boto3.client('s3')
-
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id= os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key= os.getenv('AWS_SECRET_ACCESS_KEY'),
+        region_name= os.getenv('AWS_DEFAULT_REGION')
+    )
     try:
         s3_client.put_object(
             Bucket=bucket_name,
